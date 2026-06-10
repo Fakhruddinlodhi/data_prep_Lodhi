88cfe5eca5cac118d19b2cac5a3466e22ca82895
Fakhruddin Lodhi <fakhruddinlodhi@hotmail.com>
2026-06-09 21:04:31 -0500

    The Final Deliverable

--- Queries.sql
+++ Queries.sql
@@ -1,22 +1,43 @@
 --testing
-select * from netflix_titles.csv;
-select * from read_csv('netflix_titles.csv');
+SELECT * FROM netflix_titles.csv;
+SELECT * FROM read_csv('netflix_titles.csv');
+
 
 --creating table
 CREATE TABLE netflix_content AS
 SELECT * FROM read_csv('netflix_titles.csv');
 
+
 --testing
-select * from netflix_content;
+SELECT * FROM netflix_content;
+
+
 
 --query1
-select title, director, release_year from netflix_content where country='Japan';
+SELECT  title, 
+        director, 
+        release_year 
+FROM netflix_content 
+WHERE country='Japan';
+
+
 
 --query2
-select type, count(type) from netflix_content group by type;
+SELECT  type, 
+        count(type) AS count
+FROM netflix_content 
+GROUP BY type;
+
+
 
 --query3
-select title, release_year from netflix_content order by release_year asc limit 10;
+SELECT  title, 
+        release_year 
+FROM netflix_content 
+ORDER BY release_year ASC 
+LIMIT 10;
+
+
 
 --bonus query works
 SELECT title, release_year
